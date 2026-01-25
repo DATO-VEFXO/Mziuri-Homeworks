cbe28138e0e5acc2ff6a8e73716d53145abbb0bf
DATO-VEFXO <107356352+DATO-VEFXO@users.noreply.github.com>
2026-01-25 21:10:39 +0400

    Add files via upload

---
+++ homework 14.py
@@ -0,0 +1,32 @@
+#1
+file1 = open("f1", "r")
+file2 = open("f2", "w")
+for each in file1:
+    a = int(each)
+    file2.write(str(a * a) + "\n")
+file1.close()
+file2.close()
+#2
+file = open("oscars.txt", "r")
+lines = file.readlines()
+year_input = input("sheiyvanet weli ")
+found = False
+for line in lines:
+    p = line.strip().split(",")
+    year = p[0]
+    name = p[3]
+    if year == year_input:
+        print("oskaris mflobeli", name)
+        found = True
+if not found:
+    print("am wels aravis miugia oskari")
+youngest_age = 100
+youngest_name = ""
+for line in lines:
+    p = line.strip().split(",")
+    age = int(p[2])
+    name = p[3]
+    if age < youngest_age:
+        youngest_age = age
+        youngest_name = name
+print("yvelaze axalgazrda oskaris mflobeli", "- saxeli gvari ->", youngest_name, "- asaki -> ", youngest_age)
